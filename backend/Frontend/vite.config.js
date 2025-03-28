ba25637e5eb46a0d6d0ec6cdc615b2a18b4f269d
ritik913553 <ritikgupta9135@gmail.com>
2025-03-28 14:28:11 +0530

    update API proxy target in Vite configuration for production deployment

--- backend/Frontend/vite.config.js
+++ backend/Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api' :{
-          target: 'http://localhost:8000',
+          target: 'https://weather-app-1pnj.onrender.com',
           changeOrigin:true
       }
     },
