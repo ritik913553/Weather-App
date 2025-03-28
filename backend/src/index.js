427c4692ebacf85ad823f3d1bd8cdbca7f5ca5f0
ritik913553 <ritikgupta9135@gmail.com>
2025-03-28 14:19:59 +0530

    import express in index.js for enhanced server functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import {app} from './app.js'
 import connectDB from './db/index.js'
 import path from 'path'
-
+import express from 'express'
 dotenv.config({
     path: "./env"
 })
